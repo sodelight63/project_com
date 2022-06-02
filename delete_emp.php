2672e6a1b5d032caf8d957ad31550b8aaedf3bb9
sodelight63 <624234022@parichat.skru.ac.th>
2022-06-02 18:38:42 +0700

    V.1.1

---
+++ delete_emp.php
@@ -0,0 +1,10 @@
+<?php
+    $BooKId = $_GET['employee_id'];
+    require_once 'config/connect_db.php';
+    $sql = "DELETE FROM employee_id WHERE employee_id = employee_id";
+    $stmt = $conn->prepare($sql);
+    if($stmt->execute([$employee_id])){
+        header("Location:index_emp.php");
+    }else{
+        echo "ลบไม่สำเร็จ";
+    }
